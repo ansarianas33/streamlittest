8c7e941f4778d2ae8a8a50d18f06da0e60c09b3c
ansarianas33 <ansarianas33@gmail.com>
2023-07-17 21:38:44 +0530

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,2 +1,7 @@
 import streamlit as st
 st.write("ABCD")
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
